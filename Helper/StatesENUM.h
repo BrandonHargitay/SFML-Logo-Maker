993c30b15bd771089d796bdfee2e3f6d5223a87e
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-16 12:12:55 -0700

    Add states class

---
+++ Helper/StatesENUM.h
@@ -0,0 +1,13 @@
+//
+// Created by Brandon Hargitay on 3/16/23.
+//
+
+#ifndef TEXT_INPUT_STATESENUM_H
+#define TEXT_INPUT_STATESENUM_H
+
+enum statesEnum {
+    HIDDEN,
+    LAST_STATE
+};
+
+#endif //TEXT_INPUT_STATESENUM_H
---
+++ Helper/states.h
@@ -0,0 +1,26 @@
+//
+// Created by Brandon Hargitay on 3/16/23.
+//
+
+#ifndef TEXT_INPUT_STATES_H
+#define TEXT_INPUT_STATES_H
+
+#include "statesEnum.h"
+#include <map>
+
+class states{
+private:
+
+    std::map<statesEnum, bool> map;
+    void load();
+public:
+    states();
+    bool checkState(statesEnum state) const;
+    void enableState(statesEnum state);
+    void disableState(statesEnum state);
+    void setState(statesEnum state, bool value);
+    void toggleState(statesEnum state);
+};
+
+
+#endif //TEXT_INPUT_STATES_H
