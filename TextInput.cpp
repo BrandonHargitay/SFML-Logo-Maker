38321b8930b83a33794d8c25223bd2ebc98749f5
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-16 12:59:06 -0700

    Add text input class

---
+++ TextInput.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Brandon Hargitay on 3/16/23.
+//
+
+#include "TextInput.h"
---
+++ TextInput.h
@@ -0,0 +1,15 @@
+//
+// Created by Brandon Hargitay on 3/16/23.
+//
+
+#ifndef TEXT_INPUT_TEXTINPUT_H
+#define TEXT_INPUT_TEXTINPUT_H
+#include "Typing.h"
+#include "BlinkingCursor.h"
+
+class TextInput {
+
+};
+
+
+#endif //TEXT_INPUT_TEXTINPUT_H
