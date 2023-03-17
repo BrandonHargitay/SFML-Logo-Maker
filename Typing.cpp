693c23d9101de7b377b94dcc21ac3001b395e776
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-16 18:24:16 -0700

    Add method to get x pos of last letter &&  add some code debugging outputs

--- Typing.cpp
+++ Typing.cpp
@@ -12,11 +12,10 @@ void Typing::draw(sf::RenderTarget &target, sf::RenderStates states) const {
 }
 
 Typing::Typing() {
-    setPosition({50.f, 50.f});
+    setPosition({0, 0.f});
     letter.setFont(Font::getFont());
     letter.setFillColor(sf::Color::White);
     letter.setCharacterSize(40);
-    starting_point = {0,0};
 }
 
 void Typing::addEventHandler(sf::RenderWindow &window, const sf::Event &event) {
@@ -36,19 +35,28 @@ void Typing::addEventHandler(sf::RenderWindow &window, const sf::Event &event) {
 void Typing::setCharacterPosition() {
     if (textInput.size() == 1) {
         textInput.back().setPosition(getPosition());
+        sf::Text lastText = textInput.back();
+        std::cout << "TEXT:" << std::endl;
+        std::cout << "X: " << lastText.getPosition().x << " Y: " << lastText.getPosition().y << std::endl;
     }
     else {
-        textInput.back().setPosition(std::prev(textInput.end(), 2)->getPosition().x
-                                    + std::prev(textInput.end(), 2)->getGlobalBounds().width
-                                    + std::prev(textInput.end(), 2)->getLetterSpacing()
-                                    , getPosition().y );
+        float xpos = std::prev(textInput.end(), 2)->getPosition().x
+                         + std::prev(textInput.end(), 2)->getGlobalBounds().width
+                         + std::prev(textInput.end(), 2)->getLetterSpacing();
+        textInput.back().setPosition(xpos, getPosition().y );
+        std::cout << "TEXT:" << std::endl;
+        sf::Text lastText = textInput.back();
+        std::cout << "X: " << lastText.getPosition().x << " Y: " << lastText.getPosition().y << std::endl;
+
+
     }
 }
 
 float Typing::getLastX() {
-    return std::prev(textInput.end(), 2)->getPosition().x
-           + std::prev(textInput.end(), 2)->getGlobalBounds().width
-           + std::prev(textInput.end(), 2)->getLetterSpacing();
+    if (!textInput.empty()) {
+        sf::Text lastText = textInput.back();
+        return lastText.getPosition().x + lastText.getGlobalBounds().width + lastText.getLetterSpacing();
+    }
 }
 
 float Typing::getY() {
