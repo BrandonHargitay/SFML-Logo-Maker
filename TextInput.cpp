9ec6afbb72764816bf89543d78c8a4da0d67db7b
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-16 19:50:32 -0700

    Add text box to object

--- TextInput.cpp
+++ TextInput.cpp
@@ -5,21 +5,27 @@
 #include "TextInput.h"
 
 TextInput::TextInput() {
-    setPosition(0,0);
+    Transformable::setPosition(500, 100);
     setState(HIDDEN, false);
 }
 
 void TextInput::draw(sf::RenderTarget &window, sf::RenderStates states) const {
-    window.draw(typing);
-    window.draw(cursor);
+    states.transform *= getTransform();
+    window.draw(box, states);
+    window.draw(typing, states);
+    window.draw(cursor, states);
+
 }
 
 void TextInput::update() {
     cursor.setPosition(typing.getLastX(),typing.getY());
     cursor.update();
+
 }
 
 void TextInput::addEventHandler(sf::RenderWindow &window, sf::Event event) {
     typing.addEventHandler(window, event);
     update();
 }
+
+
--- TextInput.h
+++ TextInput.h
@@ -7,16 +7,18 @@
 #include "Typing.h"
 #include "BlinkingCursor.h"
 #include "Helper/MouseEvents.h"
-
+#include "TextBox.h"
 class TextInput : public sf::Drawable, public sf::Transformable, public states{
 public:
     TextInput();
     virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
     void update();
     void addEventHandler(sf::RenderWindow& window, sf::Event event);
+    void setPosition(float x, float y);
 private:
     Typing typing;
     BlinkingCursor cursor;
+    TextBox box;
 };
 
 
