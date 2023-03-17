3ceb45a72e9e3ba78bd809c85c4ce4e0aeac5f47
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-17 15:51:33 -0700

    Add label for text box

--- TextBox.cpp
+++ TextBox.cpp
@@ -9,18 +9,34 @@ TextBox::TextBox(){
     box.setFillColor({55,55,55});
     box.setOutlineThickness(2.f);
     box.setSize({400.0f,50.0f});
+    label.setFont(Font::getFont());
+    label.setFillColor(sf::Color::White);
+
+    label.setCharacterSize(30);
+    label.setFillColor(sf::Color(255,255,255,75));
 }
 
 void TextBox::draw(sf::RenderTarget &target, sf::RenderStates states) const {
     target.draw(box);
+    target.draw(label);
 }
 
 void TextBox::setPosition(float x, float y) {
     box.setPosition(x,y);
+    label.setPosition(x,y);
 }
 
 sf::FloatRect TextBox::getGlobalBounds() const{
     return box.getGlobalBounds();
+}
+
+void TextBox::centerText() {
+    Position::centerText(box,label);
+
+}
+
+void TextBox::setString(const std::string str) {
+    label.setString(str);
 }
 
 
--- TextBox.h
+++ TextBox.h
@@ -6,16 +6,20 @@
 #define TEXT_INPUT_TEXTBOX_H
 #include "SFML/Graphics.hpp"
 #include "Helper/states.h"
-
+#include "Font.h"
+#include "Helper/Position.h"
 class TextBox :  public sf::Drawable, public states{
 public:
     TextBox();
     void setPosition(float x,  float y);
     void getPosition();
     sf::FloatRect getGlobalBounds() const;
+    void centerText();
+    void setString(const std::string str);
 private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     sf::RectangleShape box;
+    sf::Text label;
 };
 
 
