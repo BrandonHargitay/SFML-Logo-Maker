5b568cb8b4411541d04c1c456e841fd079480eab
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-16 18:23:37 -0700

    Add methods for text input

--- TextInput.cpp
+++ TextInput.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "TextInput.h"
+
+TextInput::TextInput() {
+    setPosition(0,0);
+    setState(HIDDEN, false);
+}
+
+void TextInput::draw(sf::RenderTarget &window, sf::RenderStates states) const {
+    window.draw(typing);
+    window.draw(cursor);
+}
+
+void TextInput::update() {
+    cursor.setPosition(typing.getLastX(),typing.getY());
+    cursor.update();
+}
+
+void TextInput::addEventHandler(sf::RenderWindow &window, sf::Event event) {
+    typing.addEventHandler(window, event);
+    update();
+}
--- TextInput.h
+++ TextInput.h
@@ -6,9 +6,17 @@
 #define TEXT_INPUT_TEXTINPUT_H
 #include "Typing.h"
 #include "BlinkingCursor.h"
+#include "Helper/MouseEvents.h"
 
-class TextInput {
-
+class TextInput : public sf::Drawable, public sf::Transformable, public states{
+public:
+    TextInput();
+    virtual void draw(sf::RenderTarget& window, sf::RenderStates states) const;
+    void update();
+    void addEventHandler(sf::RenderWindow& window, sf::Event event);
+private:
+    Typing typing;
+    BlinkingCursor cursor;
 };
 
 
